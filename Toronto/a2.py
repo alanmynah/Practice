bd77455ba2565b83a2f5c901c952122ed7403826
Michael <michael@leannes-mbp.home>
2017-06-03 09:07:19 +0100

    Coursera and U of T updates

---
+++ Toronto/a2.py
@@ -0,0 +1,63 @@
+def get_length(dna):
+    """ (str) -> int
+
+    Return the length of the DNA sequence dna.
+
+    >>> get_length('ATCGAT')
+    6
+    >>> get_length('ATCG')
+    4
+    """
+    return len(dna)
+
+# print (get_length('ATCGAT'))
+# print (get_length('ATCG'))
+
+def is_longer(dna1, dna2):
+    """ (str, str) -> bool
+
+    Return True if and only if DNA sequence dna1 is longer than DNA sequence
+    dna2.
+
+    >>> is_longer('ATCG', 'AT')
+    True
+    >>> is_longer('ATCG', 'ATCGGA')
+    False
+    """
+    return dna1 > dna2
+
+# print (is_longer('ATCG', 'AT'))
+# print (is_longer('ATCG', 'ATCGGA'))
+
+def count_nucleotides(dna, nucleotide):
+    """ (str, str) -> int
+
+    Return the number of occurrences of nucleotide in the DNA sequence dna.
+
+    >>> count_nucleotides('ATCGGC', 'G')
+    2
+    >>> count_nucleotides('ATCTA', 'G')
+    0
+    """
+    nucleotide_count = 0
+    for nucl in dna:
+        if nucl in nucleotide:
+            nucleotide_count += 1
+
+    return nucleotide_count
+
+# print (count_nucleotides('ATCGGC', 'G'))
+# print (count_nucleotides('ATCTA', 'G'))
+
+def contains_sequence(dna1, dna2):
+    """ (str, str) -> bool
+
+    Return True if and only if DNA sequence dna2 occurs in the DNA sequence
+    dna1.
+
+    >>> contains_sequence('ATCGGC', 'GG')
+    True
+    >>> contains_sequence('ATCGGC', 'GT')
+    False
+
+    """
---
+++ Toronto/digits.py
@@ -0,0 +1,63 @@
+digits = '0123456789'
+result = 0
+
+for digit in digits:
+    result = result + int(digit)
+
+print(result)
+
+digits = '0123456789'
+result = 0
+
+for digit in digits:
+    result = digit
+
+print(result)
+
+digits = '0123456789'
+result = ''
+
+for digit in digits:
+    result = result + digit * 2
+
+print(result)
+
+message = 'Happy 29th!'
+new_message = ''
+
+for char in message:
+    new_message = new_message + str((int(char) + 1) % 10)
+
+print(new_message)
+
+message = 'Happy 29th!'
+new_message = ''
+
+for char in message:
+    if not char.isdigit():
+        new_message = new_message + char
+    else:
+        new_message = new_message + str((int(char) + 1) % 10)
+
+print(new_message)
+
+message = 'Happy 29th!'
+new_message = ''
+
+for char in message:
+    if not char.isdigit():
+        new_message = new_message + char
+    else:
+        new_message = new_message + str((int(char) + 1) % 10)
+
+print(new_message)
+
+message = 'Happy 29th!'
+new_message = ''
+
+for char in message:
+    if char.isdigit():
+        new_message = new_message + str((int(char) + 1) % 10)
+    new_message = new_message + char
+
+print(new_message)
